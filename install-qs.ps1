bef7138f5fd8dbd9f1018505e14ba41d8209bae5
clintcarr <clint.carr@outlook.com>
2016-10-24 11:14:58 +1100

    logging
    Added basic logging to each step.

--- install-qs.ps1
+++ install-qs.ps1
@@ -6,6 +6,7 @@
 #----------------------------------
 # Variables
 #----------------------------------
+
 $serial = ''
 $control = ''
 $name = ''
@@ -16,37 +17,47 @@ $serviceAccountPass = ''
 $PostgresAccountPass = ''
 $hostname = 'FQDN'
 
-New-Item -ItemType directory -Path C:\installation\qlik-cli -force
+$date = Get-Date -format "yyyyMMddHHmm"
 
+New-Item -ItemType directory -Path C:\installation\qlik-cli -force
+"Created path: c:\installation\qlik-cli $date" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 $source = "https://da3hntz84uekx.cloudfront.net/QlikSense/3.1.1/1/_MSI/Qlik_Sense_setup.exe"
 $destination = "c:\installation\Qlik_Sense_setup.exe"
 Invoke-WebRequest $source -OutFile $destination
+"$date Downloaded Qlik_Sense_setup.exe" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 $source = "https://github.com/ahaydon/Qlik-Cli/archive/master.zip"
 $destination = "c:\installation\qlik-cli\qlik-cli.zip"
 Invoke-WebRequest $source -OutFile $destination
+"$date Downloaded qlik-cli.zip" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 $shell = New-Object -ComObject shell.application
 $zip = $shell.NameSpace("C:\installation\qlik-cli\qlik-cli.zip")
 foreach ($item in $zip.items()) {
   $shell.Namespace("c:\installation\qlik-cli").CopyHere($item)
 }
+"$date Unzipped qlik-cli" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 New-Item -ItemType directory -Path C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Qlik-Cli -force
 Copy-Item C:\Installation\qlik-cli\Qlik-Cli-master\Qlik-Cli.psm1 C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Qlik-Cli\
 Import-Module Qlik-Cli.psm1
+"$date Imported qlik-cli to PowerShell Modules" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 Write-Host "Adding service account user to local administrators group"
 ([ADSI]"WinNT://$hostname/administrators,group").psbase.Invoke("Add",([ADSI]"WinNT://$serviceAccount2").path)
+"$date Added $serviceAccount2 to local administrators group" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 Write-Host "Installing Qlik Sense Enterprise"
 Invoke-Command -ScriptBlock {Start-Process -FilePath "c:\installation\Qlik_Sense_setup.exe" -ArgumentList "-s dbpassword=$PostgresAccountPass hostname=$hostname userwithdomain=$serviceAccount password=$serviceAccountPass" -Wait -PassThru}
+"$date Installed Qlik Sense 3.1.1" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 write-host "Wait 60 seconds for Qlik Sense to intialise"
 Start-Sleep -s 60
 
 Write-Host "Connecting to Qlik Sense Repository Service"
 Connect-Qlik $hostname -UseDefaultCredentials
+"$date Connected to $hostname" | Out-File -filepath C:\installation\qsInstallLog.txt -append
 
 Write-Host "Setting license"
 Set-QlikLicense -serial $serial -control $control -name $name -organization $organization
+"$date Written license: $serial" | Out-File -filepath C:\installation\qsInstallLog.txt -append
