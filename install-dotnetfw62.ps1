fa333807497c05b3fc8b46de32aa71830e9a20bc
Clint Carr <clint.carr@outlook.com>
2016-11-04 10:34:16 +1100

    Update install-dotnetfw62.ps1

--- install-dotnetfw62.ps1
+++ install-dotnetfw62.ps1
@@ -1,19 +1,11 @@
 New-Item -ItemType directory -Path C:\installation\ -force
+$Password = Read-Host -AsSecureString
+New-LocalUser "Qservice" -Password $Password -FullName "Qlik Service Account"
 
 $source = 'http://go.microsoft.com/fwlink/?LinkId=780600'
 $destination = 'c:\installation\ndp462-kb3151800-x86-x64-AllOS-ENU.exe'
 Invoke-WebRequest $source -OutFile $destination
 
 Invoke-Command -ScriptBlock {Start-Process -FilePath "C:\installation\ndp462-kb3151800-x86-x64-AllOS-ENU.exe" -ArgumentList "/q" -Wait -PassThru}
-
-$source = 'https://github.com/clintcarr/qlik-sense-automated-install/archive/master.zip'
-$destination = 'c:\installation\master.zip'
-Invoke-WebRequest $source -OutFile $destination
-
-Expand-Archive C:\installation\master.zip -dest C:\installation\
-
-Write-Host "Changing RunOnce script." 
-$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-set-itemproperty $RunOnceKey "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + "C:\Installation\qlik-sense-automated-install-master\install-qs-ps5.ps1")
 
 shutdown -t 0 -r -f
